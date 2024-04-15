258f687e2970232e19fba4f7f0dbab1cc8a74da8
alperenf <sakaryalperenn@gmail.com>
2024-04-15 12:13:42 +0200

    Implementing Cycle Based Crossover 'WIP'

--- src/chromosome.h
+++ src/chromosome.h
@@ -5,7 +5,7 @@
 
 class Chromosome{
     public:
-        Chromosome();
+        Chromosome(const unsigned int& size = 0);
         Chromosome(const std::vector<unsigned int>& genes);
 
         void AddGene(unsigned int gene);
@@ -13,13 +13,17 @@ class Chromosome{
         void CalculateFitnessScore();
 
         long double GetFitnessScore() const { return fitnessScore; }
-        unsigned int GetGene(const unsigned int& index) const { return genes.at(index); } 
+        unsigned int GetGene(const unsigned int& index) const { return genes.at(index); }
+        std::vector<unsigned int>& GetGenes() const { return genes; };
         unsigned int GetSize() const { return static_cast<unsigned int>(genes.size()); }
 
         void PrintGenes() const;
         void ShuffleGenes();
 
         void SetGene(const unsigned int& index, const unsigned int& gene) { genes.at(index) = gene; }
+
+        const auto begin() -> std::vector<unsigned int>::iterator { return genes.begin(); }
+        const auto end() -> std::vector<unsigned int>::iterator { return genes.end(); }
 
     private:
         std::vector<unsigned int> genes;
