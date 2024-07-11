86f6b0bc1fe5a3db402bfca63652025334af88d0
Alperen <sakaryalperenn@gmail.com>
2024-07-11 22:05:33 +0300

    Updated tsp_ga_config.h

--- src/tsp_ga_config.h
+++ src/tsp_ga_config.h
@@ -6,12 +6,12 @@ struct TspGaConfig{
     float bestChromosomesDecreaseRate = 0.00f;
 
     float restChromosomesPct = 0.007f; // Rest of the chromosomes will be transferred to next generation randomly with this percentage
-    float eliteChromosomesPct = 0.005f;
+    float eliteChromosomesPct = 0.001f;
     unsigned int maxGenerations = 500;
 
     float mutationPatience = 3;
-    float mutationRate = 0.01f;
-    float mutationIncreaseRate = 0.001f;
+    float mutationRate = 0.015f;
+    float mutationIncreaseRate = 0.005f;
 
     unsigned int startPointId = 24;
 };
